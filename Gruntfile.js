c0c23df352374458c30bff3bce03282a12f087d8
Ben Sayers <bsayers@atlassian.com>
2014-03-01 17:16:39 +1100

    Updated release build to use a distribution folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,19 +60,36 @@ module.exports = function (grunt) {
         release: {
             options: {
                 file: 'bower.json',
-                npm: false
+                npm: false,
+                push: false
+            }
+        },
+        copy: {
+            release: {
+                files: {expand: true, src: ['build/**'], dest: 'dist/'}
+            }
+        },
+        exec: {
+            commitAndPushRelease: {
+                cmd: 'git add --all; git commit --amend -C HEAD; git push'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-release');
 
     grunt.registerTask('build', ['jshint:source', 'requirejs:combined', 'requirejs:minified', 'jshint:build'])
     grunt.registerTask('test', ['karma:unit']);
     grunt.registerTask('watch', ['karma:watch']);
+    grunt.registerTask('deploy', ['deploy:patch']);
+    grunt.registerTask('deploy:patch', ['build', 'test', 'copy:release', 'release:patch', 'exec:commitAndPushRelease']);
+    grunt.registerTask('deploy:minor', ['build', 'test', 'copy:release', 'release:minor', 'exec:commitAndPushRelease']);
+    grunt.registerTask('deploy:major', ['build', 'test', 'copy:release', 'release:major', 'exec:commitAndPushRelease']);
 
     grunt.registerTask('default', ['test']);
 };
