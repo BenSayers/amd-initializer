f97a5b14013688e0536493f3a418d73827a73388
Ben Sayers <bsayers@atlassian.com>
2014-03-04 23:53:57 +1100

    Fix bug where modules can be loaded twice

--- test/amdInitializer.spec.js
+++ test/amdInitializer.spec.js
@@ -1,7 +1,7 @@
 define(['squire', 'jquery'], function (Squire, $) {
     describe('amdInitializer', function () {
         var amdInitializer;
-        var modulesLoadedPromise;
+        var initializerApi;
         var module1;
         var module2;
 
@@ -26,20 +26,24 @@ define(['squire', 'jquery'], function (Squire, $) {
             });
         });
 
+        afterEach(function () {
+            initializerApi.unload()
+        });
+
         var insertIntoDom = function (markup) {
             setFixtures('<div class="target">' + markup + '</div>');
             return $('.target');
         };
 
         var invokeLoad = function (selector) {
-            modulesLoadedPromise = amdInitializer.load({ selector: selector });
+            initializerApi = amdInitializer.load({ selector: selector });
         };
 
         it('should call load on the loaded module', function (done) {
             var $target = insertIntoDom('<div class="module" data-module-name="path/to/module1"></div>');
             invokeLoad('.module');
 
-            modulesLoadedPromise.then(function () {
+            initializerApi.initialModulesLoaded.then(function () {
                 expect(module1.load).toHaveBeenCalled();
                 var loadArgs = module1.load.calls.mostRecent().args;
                 expect(loadArgs[0][0]).toBe($target.find('.module')[0], 'the element passed to the module');
@@ -52,7 +56,7 @@ define(['squire', 'jquery'], function (Squire, $) {
             insertIntoDom('<div class="module" data-module-name="path/to/module1" data-parameter-one="param-1" data-parameter-two="param2"></div>');
             invokeLoad('.module');
 
-            modulesLoadedPromise.then(function () {
+            initializerApi.initialModulesLoaded.then(function () {
                 expect(module1.load).toHaveBeenCalledWith(jasmine.any(Object), {
                     parameterOne: 'param-1',
                     parameterTwo: 'param2'
@@ -61,11 +65,21 @@ define(['squire', 'jquery'], function (Squire, $) {
             });
         });
 
+        it('should raise an event when the module has loaded', function (done) {
+            insertIntoDom('<div class="module" data-module-name="path/to/module1"></div>');
+            invokeLoad('.module');
+
+            initializerApi.onModuleLoaded(function (moduleDetails) {
+                expect(moduleDetails.name).toBe('path/to/module1');
+                done();
+            });
+        });
+
         it('should load multiple modules', function (done) {
             insertIntoDom('<div class="module" data-module-name="path/to/module1"></div><div class="module" data-module-name="path/to/module2"></div>');
             invokeLoad('.module');
 
-            modulesLoadedPromise.then(function () {
+            initializerApi.initialModulesLoaded.then(function () {
                 expect(module1.load).toHaveBeenCalled();
                 expect(module2.load).toHaveBeenCalled();
                 done();
@@ -77,7 +91,7 @@ define(['squire', 'jquery'], function (Squire, $) {
             module1.load.and.throwError('module1 failed to load');
             invokeLoad('.module');
 
-            modulesLoadedPromise.then(function () {
+            initializerApi.initialModulesLoaded.then(function () {
                 expect(module1.load).toHaveBeenCalled();
                 expect(module2.load).toHaveBeenCalled();
                 done();
@@ -89,11 +103,29 @@ define(['squire', 'jquery'], function (Squire, $) {
             invokeLoad('.module');
             $target.append('<div class="module" data-module-name="path/to/module1"></div>');
 
-            modulesLoadedPromise.then(function (api) {
-                api.onModuleLoaded(function () {
-                    expect(module1.load.calls.count()).toBe(1, 'the number of times module1.load gets called');
-                    done();
-                });
+            initializerApi.onModuleLoaded(function () {
+                expect(module1.load.calls.count()).toBe(1, 'the number of times module1.load gets called');
+                done();
+            });
+        });
+
+        it('should mark modules that have been loaded with a data-module-loaded attribute', function (done) {
+            var $target = insertIntoDom('<div class="module" data-module-name="path/to/module1"></div>');
+            invokeLoad('.module');
+
+            initializerApi.initialModulesLoaded.then(function () {
+                expect($target.find('.module')).toHaveAttr('data-module-loaded', 'true');
+                done();
+            });
+        });
+
+        it('should not load modules marked with the data-module-loaded attribute', function (done) {
+            insertIntoDom('<div class="module" data-module-name="path/to/module1" data-module-loaded="true"></div>');
+            invokeLoad('.module');
+
+            initializerApi.initialModulesLoaded.then(function () {
+                expect(module1.load).not.toHaveBeenCalled();
+                done();
             });
         });
     });
